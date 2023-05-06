47d37c033e154722f91f41fa0e1be8573cfd1c19
karakosenur <karakosenur@gmail.com>
2023-05-06 21:36:48 +0300

    v1

---
+++ src/Practice01/C01_ortalamaNotHesaplama.java
@@ -0,0 +1,42 @@
+package Practice01;
+
+import java.util.Scanner;
+
+public class C01_ortalamaNotHesaplama {
+    public static void main(String[] args) {
+
+        double mat, fizik, kimya, tarih, muzik;
+
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("Matematik notunuzu giriniz : ");
+        mat=scan.nextDouble();
+
+        System.out.println("Fizik notunuzu giriniz : ");
+        fizik=scan.nextDouble();
+
+        System.out.println("Kimya notunuzu giriniz : ");
+        kimya=scan.nextDouble();
+
+        System.out.println("Muzik notunuzu giriniz : ");
+        muzik=scan.nextDouble();
+
+        System.out.println("Tarih notunuzu giriniz : ");
+        tarih=scan.nextDouble();
+
+        /*
+        echo "# javaProjectPractice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/karakosenur/javaProjectPractice.git
+git push -u origin main
+         */
+
+
+
+
+
+    }
+}
