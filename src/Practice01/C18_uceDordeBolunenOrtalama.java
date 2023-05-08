54fc6eb5681618fe1706b9d97c02c653708a2b45
karakosenur <karakosenur@gmail.com>
2023-05-08 11:57:18 +0300

    v1

---
+++ src/Practice01/C18_uceDordeBolunenOrtalama.java
@@ -0,0 +1,33 @@
+package Practice01;
+
+import java.util.Scanner;
+
+public class C18_uceDordeBolunenOrtalama {
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lutfen bir sayi giriniz");
+        int sayi=scan.nextInt();
+        int sayac=0;
+        int toplam=0;
+
+        for (int i = 1; i <sayi ; i++) {
+
+            if (i%3==0 && i%4==0){
+                toplam+=i;
+                sayac++;
+
+            }
+        }
+        System.out.println("Gırdıgınız sayiya kadar 3 ve 4'e tam bolunen sayilarin toplami : "+toplam);
+
+        if (sayac == 0) {
+            System.out.println("Gırdıgınız sayiya kadar 3 ve 4'e tam bolunen bir sayi yok.");
+        } else {
+            double ortalama = (double) toplam / sayac;
+            System.out.println("Gırdıgınız sayiya kadar 3 ve 4'e tam bolunen sayiların ortalamasi: " + ortalama);
+        }
+
+
+    }
+}
---
+++ src/Practice01/fags.java
@@ -0,0 +1,19 @@
+package Practice01;
+
+public class fags {
+    public static void main(String[] args) {
+        /*for (int i = 1; i <= 2; ++i) {
+            for (int j = 1; j < 4; ++j) {
+                if (i == 1) continue;
+                System.out.print(i + j);
+            }
+        }
+
+         */
+        for (int k = 0; k < 9; k++) {
+            System.out.print("*");
+            if (k == 3)
+                break;
+        }
+    }
+}
